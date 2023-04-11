91863082b6b1763f5d99f4d2c5012d6091716768
Bruno Cayres Messias <brunocmessias@hotmail.com>
2023-04-11 14:48:56 -0300

    adding meshgrid indexing pro warming, and moving train.py

--- engine/loss.py
+++ engine/loss.py
@@ -15,7 +15,7 @@ def coords_fmap2orig(feature,stride): #TODO: Move this to utils file
     shifts_x = torch.arange(0, w * stride, stride, dtype=torch.float32)
     shifts_y = torch.arange(0, h * stride, stride, dtype=torch.float32)
 
-    shift_y, shift_x = torch.meshgrid(shifts_y, shifts_x)
+    shift_y, shift_x = torch.meshgrid(shifts_y, shifts_x, 'ij')
     shift_x = torch.reshape(shift_x, [-1])
     shift_y = torch.reshape(shift_y, [-1])
     coords = torch.stack([shift_x, shift_y], -1) + stride // 2
