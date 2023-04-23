d3900068d6aacc71edd72ccf0fc346bac507d0f3
Bruno Cayres Messias <brunocmessias@hotmail.com>
2023-04-23 12:22:06 -0300

    changing vocDataset

--- data/voc.py
+++ data/voc.py
@@ -1,12 +1,23 @@
+import torch
+import xml.etree.ElementTree as ET
 import os
 import cv2
-import torch
 import numpy as np
-import xml.etree.ElementTree as ET
-
 from torchvision import transforms
+from PIL import  Image
+import random
 
-class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dataset
+def flip(img, boxes):
+    img = img.transpose(Image.FLIP_LEFT_RIGHT)
+    w = img.width
+    if boxes.shape[0] != 0:
+        xmin = w - boxes[:,2]
+        xmax = w - boxes[:,0]
+        boxes[:, 2] = xmax
+        boxes[:, 0] = xmin
+    return img, boxes
+
+class VOCDataset(torch.utils.data.Dataset):
     CLASSES_NAME = (
         "__background__ ",
         "aeroplane",
@@ -30,7 +41,7 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
         "train",
         "tvmonitor",
     )
-    def __init__(self,root_dir,resize_size=[800,1024],split='trainval',use_difficult=False):
+    def __init__(self,root_dir,resize_size=[800,1333],split='trainval',use_difficult=False,is_train = True, augment = None):
         self.root=root_dir
         self.use_difficult=use_difficult
         self.imgset=split
@@ -43,21 +54,21 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
             self.img_ids=f.readlines()
         self.img_ids=[x.strip() for x in self.img_ids]
         self.name2id=dict(zip(VOCDataset.CLASSES_NAME,range(len(VOCDataset.CLASSES_NAME))))
+        self.id2name = {v:k for k,v in self.name2id.items()}
         self.resize_size=resize_size
         self.mean=[0.485, 0.456, 0.406]
         self.std=[0.229, 0.224, 0.225]
+        self.train = is_train
+        self.augment = augment
         print("INFO=====>voc dataset init finished  ! !")
 
     def __len__(self):
         return len(self.img_ids)
 
-    def _read_img_rgb(self,path):
-        return cv2.cvtColor(cv2.imread(path),cv2.COLOR_BGR2RGB)
-
     def __getitem__(self,index):
-        
+
         img_id=self.img_ids[index]
-        img=self._read_img_rgb(self._imgpath%img_id)
+        img = Image.open(self._imgpath%img_id)
 
         anno=ET.parse(self._annopath%img_id).getroot()
         boxes=[]
@@ -70,9 +81,9 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
             # Make pixel indexes 0-based
             # Refer to "https://github.com/rbgirshick/py-faster-rcnn/blob/master/lib/datasets/pascal_voc.py#L208-L211"
             box=[
-                _box.find("xmin").text, 
-                _box.find("ymin").text, 
-                _box.find("xmax").text, 
+                _box.find("xmin").text,
+                _box.find("ymin").text,
+                _box.find("xmax").text,
                 _box.find("ymax").text,
             ]
             TO_REMOVE=1
@@ -83,8 +94,14 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
 
             name=obj.find("name").text.lower().strip()
             classes.append(self.name2id[name])
-        
+
         boxes=np.array(boxes,dtype=np.float32)
+        if self.train:
+            if random.random() < 0.5:
+                img, boxes = flip(img, boxes)
+            if self.augment is not None:
+                img, boxes = self.augment(img, boxes)
+        img = np.array(img)
         img,boxes=self.preprocess_img_boxes(img,boxes,self.resize_size)
 
         img=transforms.ToTensor()(img)
@@ -92,13 +109,13 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
         classes=torch.LongTensor(classes)
 
         return img,boxes,classes
-        
+
 
     def preprocess_img_boxes(self,image,boxes,input_ksize):
         '''
-        resize image and bboxes 
+        resize image and bboxes
         Returns
-        image_paded: input_ksize  
+        image_paded: input_ksize
         bboxes: [None,4]
         '''
         min_side, max_side    = input_ksize
@@ -121,8 +138,8 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
         if boxes is None:
             return image_paded
         else:
-            boxes[:, [0, 2]] = boxes[:, [0, 2]] * scale 
-            boxes[:, [1, 3]] = boxes[:, [1, 3]] * scale 
+            boxes[:, [0, 2]] = boxes[:, [0, 2]] * scale
+            boxes[:, [1, 3]] = boxes[:, [1, 3]] * scale
             return image_paded, boxes
     def collate_fn(self,data):
         imgs_list,boxes_list,classes_list=zip(*data)
@@ -140,14 +157,16 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
             img=imgs_list[i]
             pad_imgs_list.append(transforms.Normalize(self.mean, self.std,inplace=True)(torch.nn.functional.pad(img,(0,int(max_w-img.shape[2]),0,int(max_h-img.shape[1])),value=0.)))
 
+
         max_num=0
         for i in range(batch_size):
             n=boxes_list[i].shape[0]
-            if n>max_num:max_num=n   
+            if n>max_num:max_num=n
         for i in range(batch_size):
             pad_boxes_list.append(torch.nn.functional.pad(boxes_list[i],(0,0,0,max_num-boxes_list[i].shape[0]),value=-1))
             pad_classes_list.append(torch.nn.functional.pad(classes_list[i],(0,max_num-classes_list[i].shape[0]),value=-1))
-        
+
+
         batch_boxes=torch.stack(pad_boxes_list)
         batch_classes=torch.stack(pad_classes_list)
         batch_imgs=torch.stack(pad_imgs_list)
@@ -156,18 +175,33 @@ class VOCDataset(torch.utils.data.Dataset): #TODO: Inherent from torchvision dat
 
 
 if __name__=="__main__":
-    import cv2
-    
-    dataset=VOCDataset("/home/data/voc2007_2012/VOCdevkit/VOC2012",split='trainval')
-
-    imgs,boxes,classes=dataset.collate_fn([dataset[105],dataset[101],dataset[200]])
-    print(boxes,classes,"\n",imgs.shape,boxes.shape,classes.shape,boxes.dtype,classes.dtype,imgs.dtype)
-    for index,i in enumerate(imgs):
-        i=i.numpy().astype(np.uint8)
-        i=np.transpose(i,(1,2,0))
-        i=cv2.cvtColor(i,cv2.COLOR_RGB2BGR)
-        print(i.shape,type(i))
-        cv2.imwrite(str(index)+".jpg",i)
+    pass
+    eval_dataset = VOCDataset(root_dir='/Users/VOCdevkit/VOCdevkit/VOC0712', resize_size=[800, 1333],
+                               split='test', use_difficult=False, is_train=False, augment=None)
+    print(len(eval_dataset.CLASSES_NAME))
+    #dataset=VOCDataset("/home/data/voc2007_2012/VOCdevkit/VOC2012",split='trainval')
+    # for i in range(100):
+    #     img,boxes,classes=dataset[i]
+    #     img,boxes,classes=img.numpy().astype(np.uint8),boxes.numpy(),classes.numpy()
+    #     img=np.transpose(img,(1,2,0))
+    #     print(img.shape)
+    #     print(boxes)
+    #     print(classes)
+    #     for box in boxes:
+    #         pt1=(int(box[0]),int(box[1]))
+    #         pt2=(int(box[2]),int(box[3]))
+    #         img=cv2.rectangle(img,pt1,pt2,[0,255,0],3)
+    #     cv2.imshow("test",img)
+    #     if cv2.waitKey(0)==27:
+    #         break
+    #imgs,boxes,classes=eval_dataset.collate_fn([dataset[105],dataset[101],dataset[200]])
+    # print(boxes,classes,"\n",imgs.shape,boxes.shape,classes.shape,boxes.dtype,classes.dtype,imgs.dtype)
+    # for index,i in enumerate(imgs):
+    #     i=i.numpy().astype(np.uint8)
+    #     i=np.transpose(i,(1,2,0))
+    #     i=cv2.cvtColor(i,cv2.COLOR_RGB2BGR)
+    #     print(i.shape,type(i))
+    #     cv2.imwrite(str(index)+".jpg",i)
 
 
 
